438c49a0ed9f4f7313f8d7f83bee661c7b36e107
Yiduo Ke <yiduoke822@jhs185.org>
2017-10-27 13:51:57 -0400

    did random dev whatever thing

--- devRandom.c
+++ devRandom.c
@@ -1,9 +1,39 @@
-#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include  <fcntl.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
-int main (){
-    fd = open(“dev/random”, O_RDONLY, 0600);
+int Random(){
+    int i = open("/dev/random", O_RDONLY);
+    int margaret;
+    
+    read(i, &margaret, sizeof(int));
+    
+    if(errno){
+        printf("Errno: %d\n", errno);
+        printf("Error Message: %s\n", strerror(errno));
+    }
 
+    close(i);
+    return margaret;
+}
+
+int main(){
+    int array[10];
+    int i;
+    for (i=0; i<10; i++){
+        array[i]=Random();
+        printf("%d\n",array[i]);
+    } 
+
+    int fd = open("array", O_CREAT);
+    
+    int fd1 = write(fd, &array, sizeof(int)*10);
+    if (errno){
+        printf("Errno: %d\n", errno);
+        printf("Error Message: %s\n", strerror(errno));
+    }
+    return 0;
 }
